c2ce5483dfb8dae4dc016f5318f15db77dc512cd
Rinaldo Benaccetta <benaccettarinaldo@gmail.com>
2022-09-15 11:16:42 +0200

    put vendor to git!

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'b1477b021b12c6d5d25082b66580017d040224e7',
+        'name' => 'rinaldo/hackers_poulette',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'rinaldo/hackers_poulette' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'b1477b021b12c6d5d25082b66580017d040224e7',
+            'dev_requirement' => false,
+        ),
+    ),
+);
